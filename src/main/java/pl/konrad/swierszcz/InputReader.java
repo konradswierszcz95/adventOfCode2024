273c4d3ddb558d34f45417f49e138b36de802b59
konradswierszcz95 <konradswierszcz@mail.com>
2024-12-07 14:12:57 +0100

    fix input reader

--- src/main/java/pl/konrad/swierszcz/InputReader.java
+++ src/main/java/pl/konrad/swierszcz/InputReader.java
@@ -5,12 +5,13 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 
 public class InputReader {
     public static List<String> readInput(String path) {
         try {
             var filePath = Paths.get(
-                    InputReader.class.getClassLoader().getResource(path)
+                    Objects.requireNonNull(InputReader.class.getClassLoader().getResource(path))
                             .toURI());
             return Files.readAllLines(filePath);
         } catch (IOException | URISyntaxException e) {
